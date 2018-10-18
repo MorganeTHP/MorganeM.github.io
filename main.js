1ef8ea467d53b50ae8e813941a4d74986e3d7223
MorganeTHP <43853130+MorganeTHP@users.noreply.github.com>
2018-10-19 01:33:10 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function messagePopUp(){
+var ok = confirm('Hey ça te dit de faire un tour sur xxxvidsxxx ?')
+	if (ok == true)
+	{
+		document.location.href= "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+	}
+}
+setTimeout(messagePopUp, 10000);
+
+
